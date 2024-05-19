99eb074d63ee78046fd7320c08a5f890c8f90aa4
Tony Vardin <anthonyvardin@yahoo.com>
2024-05-19 11:46:01 -0500

    first commit

---
+++ ReactWithASP.Server/Cookie.cs
@@ -0,0 +1,12 @@
+﻿namespace ReactWithASP.Server
+{
+    public class Cookie
+    {
+      public string? Name { get; set; }
+
+      public string? Desc { get; set; }
+
+      public double Price { get; set; }
+
+    }
+}
