67f2ebca40950cf11799e4e819bf1d9bc2bd61fe
Tony Vardin <anthonyvardin@yahoo.com>
2024-06-14 12:01:05 -0500

    Updating records

--- reactwithasp.client/src/AppCookie.jsx
+++ reactwithasp.client/src/AppCookie.jsx
@@ -1,31 +1,79 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import CookieSubmit from './CookieSubmit'
+import axios from 'axios'
 
+
+const hl = "highlighted-input";
 
 function AppCookie() {
-    const [cookies, setCookies] = useState();
 
+    const [cookies, setCookies] = useState()
+   
+    const [changed, setChanged] = useState({
+        name: {},
+        desc: {},
+        price: {},
+    });
+   
     
-
-    const onChangeInput = (e, id) => {
+    const onChangeInput = (e,column,  id) => {
         const { name, value } = e.target
        
-      
+        setChanged((allChanged) => ({
+            ...allChanged,
+            [column]: {
+                ...allChanged[column],
+                [id]: true,
+            },
+
+        }));
+
         const editData = cookies.map((cookie) =>
             cookie.id === id && name ? { ...cookie, [name]: value } : cookie
         )
-
         setCookies(editData)
     }
 
+    const handleClick = async (event) => {
+       
+        const id = event.cookie.id
+      
+        setChanged((allChanged) => ({
+            "name": {
+                ...allChanged["name"],
+                [id]: false,
+            },
+            "desc": {
+                ...allChanged["desc"],
+                [id]: false,
+            },
+            "price": {
+                ...allChanged["price"],
+                [id]: false,
+            }
+
+        }));
+
+        const response = await axios({
+            method: 'post',
+            url: '/cookie/UpdateCookie',
+            headers: { 'Content-Type': 'application/json' },
+            data: event.cookie,
+            dataType: 'json'
+        });
+
+        setCookies(response.data)
+
+    }
 
    useEffect(() => {
         populateCookieData();
     }, []);
 
     const contents = cookies === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. </em></p>
+        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. </em></p> 
+       
         : 
         <table className="table table-striped" aria-labelledby="tabelLabel">
             <thead>
@@ -42,27 +90,33 @@ function AppCookie() {
                     <tr key={cookie.id}>
                         <td>{cookie.id}</td>
                         <td>{cookie.date}</td>
-                        <td><input
+                        <td>
+                         <input
                             name="name"
                             value={cookie.name}
                             type="text"
-                            onChange={(e) => onChangeInput(e, cookie.id)}
-                            placeholder="Type Name" />
+                            onChange={(e) => onChangeInput(e, "name", cookie.id)}
+                            placeholder="Type Name"
+                            className={changed.name[cookie.id] ? hl : ""}/>
                         </td>
-                        <td><input
+                        <td> <input
                             name="desc"
                             value={cookie.desc}
                             type="text"
-                            onChange={(e) => onChangeInput(e, cookie.id)}
-                            placeholder="Type Desc" /></td>
-                        <td><input
+                            onChange={(e) => onChangeInput(e,"desc", cookie.id)}
+                            placeholder="Type Desc"
+                            className={changed.desc[cookie.id] ? hl : ""} />
+                        </td>
+                         <td><input
                             name="price"
                             value={cookie.price}
                             type="text"
-                            onChange={(e) => onChangeInput(e, cookie.id)}
-                            placeholder="Type Price" /></td>
-                        <td>  <button type="submit">Update</button></td>
-
+                            onChange={(e) => onChangeInput(e,"price", cookie.id)}
+                            placeholder="Type Price"
+                            className={changed.price[cookie.id] ? hl : ""}/>
+                        </td>
+                        <td>  <button type="submit" onClick={() => handleClick({cookie})}>Update</button></td>
+                        
                     </tr>
                 )}
             </tbody>
